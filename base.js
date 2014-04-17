fb4961d4a9acfc0d0cac97b4f7c5e1308398a331
targetkiller <369160020@qq.com>
2014-04-18 02:46:15 +0800

    晚上备份
    更改点击次序，增加提示框出现情况。

--- base.js
+++ base.js
@@ -4,55 +4,87 @@
  * @date:2014-04-14;
  * @content:基础控制;
 */
+var bgChangeVelocity = 800;//背景切换速度
+var pgChangeVelocity = 800;//页面切换速度
+var showPageWait = 200;//页面停顿时间
+var inputTextVelocity = 200;//登录框打字速度
+var infoShowVelocity = 400;//提示框显示速度
+var infoShowDuration = 5000;//提示框持续时间
+
+//调试用速度
+// var bgChangeVelocity = 0;
+// var pgChangeVelocity = 0;
+// var showPageWait = 0;
+// var inputTextVelocity = 0;
+// var infoShowVelocity = 0;
+// var infoShowDuration = 0;
+
 var nowPageIndex = 1;
 var nowBgIndex = 1;
-var bgChangeVelocity = 800;//背景切换速度
 var bgChangeOffset = 200;//背景切换位移
-var pgChangeVelocity = 800;//页面切换速度
 var pgChangeOffset = 280;//页面切换位移
-var finIndex = 35;//最后帧
-var showPageWait = 1000;//页面停顿时间
+var finIndex = 33;//最后帧
 var $opt = $('#opt');
 var $hand = $('#hand');
 var $clickdot = $('#clickdot');
 
 // 开始按钮
 $('.begin-btn').click(function(){
-	// var autoFrame = setInterval(function(){nextFrame();},1000);
 	$(this).addClass('hide');
 	start();
 });
 
 // 展示开始
 function start(){
-	// 开始->移动到帧 pgChangeVelocity
-	// 初始位置 l888 t320 结束位置 l526 t320
-	handMove('526px','+=0',pgChangeVelocity,0,0,function(){
-		// 帧1->帧2 用时pgChangeVelocity
-		// 初始位置 l526 t320 结束为止 l406 t320
+	// 开始->移动到帧
+	handMove('375px','580px',800,0,1,function(){
+		handReset('+=0','+=0');
+		// 帧1->帧2
 		nextFrame(0);
-		handMove('406px','+=0',pgChangeVelocity,0,1,function(){
-			handReset('526px','+=0');
-			// 帧2->帧3 用时pgChangeVelocity
-			// 初始位置 l526 t320 结束为止 l406 t320
+		handMove('526px','320px',800,pgChangeVelocity,1,function(){
+			handReset('+=0','+=0');
+			// 帧2->帧3
 			nextFrame(showPageWait);
 			handMove('406px','+=0',pgChangeVelocity,showPageWait,1,function(){
 				handReset('526px','+=0');
-				// 帧3->帧4 用时pgChangeVelocity
-				// 初始位置 l526 t320 结束为止 l406 t320
+				// 帧3->帧4
 				nextFrame(showPageWait);
 				handMove('406px','+=0',pgChangeVelocity,showPageWait,1,function(){
 					handReset('526px','+=0');
-					// 帧4->帧5 用时pgChangeVelocity 
-					// 初始位置 l526 t320 结束为止 l406 t320
+					// 帧4->帧5
 					nextFrame(showPageWait);
 					handMove('406px','+=0',pgChangeVelocity,showPageWait,1,function(){
 						handReset('526px','+=0');
-						// 帧5->帧6 用时pgChangeVelocity
-						// 初始位置 l526 t320 结束为止 l406 t320
+						// 帧5->帧6
 						nextFrame(showPageWait);
 						handMove('406px','+=0',pgChangeVelocity,showPageWait,1,function(){
 							handReset('526px','+=0');
+							handMove('566px','+=10',pgChangeVelocity,0,1,function(){
+								handReset('+=0','+=0');
+								pg6input1TextIn();
+								handMove('566px','+=40',200,inputTextVelocity*9,1,function(){
+									handReset('+=0','+=0');
+									pg6input2TextIn();
+									handMove('566px','430px',200,inputTextVelocity*6,1,function(){
+										handReset('+=0','+=0');
+										// 帧6->帧7
+										nextFrame();
+										handMove('416px','400px',800,pgChangeVelocity,1,function(){
+											handReset('+=0','+=0');
+											// 帧7->帧8
+											nextFrame(0);
+											handMove('601px','165px',800,pgChangeVelocity,1,function(){
+												handReset('+=0','+=0');
+												// 帧8->帧8info->帧8
+												pginfoShow(infoShowDuration);
+												handMove('480px','405px',800,infoShowDuration+infoShowVelocity*2,1,function(){
+													handReset('+=0','+=0');
+												});
+											});
+										});
+									});
+								});
+							});
 						});
 					});
 				});
@@ -61,12 +93,57 @@ function start(){
 	});
 }
 
+// 显示提示页 pg8,pg15
+// d:duration
+function pginfoShow(d){
+	var _d = d||0;
+	// 显示提示页
+	var $pginfo = $('.page'+nowPageIndex+'info');
+	$pginfo.slideDown(infoShowVelocity,function(){handReset('+=0','+=0');});
+	$pginfo.delay(d).slideUp(infoShowVelocity);
+}
+
+// 登录框输入账号
+function pg6input1TextIn(){
+	var $pg6input1 = $('.pg6-input');
+	$pg6input1.focus();
+	var accountNum = 9;
+	var accountIndex = 0;
+	var account = ['3','6','9','1','6','0','0','2','0'];
+	var accountInter = setInterval(function(){
+		if(accountIndex == (accountNum-1)){
+			clearInterval(accountInter);
+		}
+		var text = $pg6input1.val()+account[accountIndex];
+		$pg6input1.val(text);
+		accountIndex++;
+	},inputTextVelocity);
+}
+
+// 等路况输入密码
+function pg6input2TextIn(){
+	var $pg6input2 = $('.pg6-input2');
+	$pg6input2.focus();
+	var passwdNum = 6;
+	var passwdIndex = 0;
+	var password = '*';
+	var passwdInter = setInterval(function(){
+		if(passwdIndex == (passwdNum-1)){
+			clearInterval(passwdInter);
+		}
+		var text = $pg6input2.val()+password;
+		$pg6input2.val(text);
+		passwdIndex++;
+	},inputTextVelocity);
+}
+
 // 移动操作手
 // l:left,t:top,d:duration,dl:delay,dot:1/0 是否显示点
 function handMove(l,t,d,dl,dot,callback){
 	$opt.removeClass('hide');
 	if(dot==1){
-		$clickdot.removeClass('hide');
+		// $clickdot.removeClass('hide');
+		$clickdot.css('opacity',1);
 	}
 	var _l = l||'+=0';
 	var _t = t||'+=0';
@@ -88,8 +165,9 @@ function handMove(l,t,d,dl,dot,callback){
 function handReset(l,t,callback){
 	var _l = l||'+=0';
 	var _t = t||'+=0';
-	$opt.addClass('hide');
+	// $opt.addClass('hide');
 	$opt.css({left:_l,top:_t,opacity:0.5})
+	$clickdot.css('opacity',0);
 	$clickdot.addClass('hide');
 }
 
